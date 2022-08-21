61a230132056a79de85832e86b31936a638c464a
Elijah <bundielly@gmail.com>
2022-08-21 03:06:56 +0300

    assignment complete

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,21 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+    include FancyDance::InstanceMethods
+    extend FancyDance::ClassMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+angelina = Dancer.new('Angelina')
+puts angelina.name
+puts angelina.twirl
+
+puts Dancer.metadata
+
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,21 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Kid 
+    include FancyDance::InstanceMethods
+    extend FancyDance::ClassMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end 
+
+tony = Kid.new('Tony')
+puts tony.name
+puts tony.take_a_bow
+
+puts Kid.metadata
+
